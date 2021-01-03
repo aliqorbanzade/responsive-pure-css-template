09af6f57047c2ba7353448a1f9b37eb480abf022
aliqorbanzade <aliqorbanzade75@gmail.com>
2021-01-03 20:37:47 +0330

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function myfunction(){
+    var x=document.getElementById('mynav')
+    if(x.className === 'nav'){
+        x.className += ' responsive'
+    }
+    else{
+        x.className ='nav'
+    }
+}
+
